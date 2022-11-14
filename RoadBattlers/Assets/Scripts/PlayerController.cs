8338667e1db9f2b717f570b2c148ced6d1c0c870
Minty B <111585812+MintyTheCoder@users.noreply.github.com>
2022-11-14 15:52:26 -0500

    Update Input and Player Controller

--- RoadBattlers/Assets/Scripts/PlayerController.cs
+++ RoadBattlers/Assets/Scripts/PlayerController.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
-    private float horizontalMove;
+    private Vector2 horizontalMove;
     public float speed = 0f;
     private float jumpingPower = 8f;
+
     public Rigidbody2D rb1;
     public Transform groundCheck;
     public LayerMask groundLayer;
@@ -17,29 +18,27 @@ public class PlayerController : MonoBehaviour
         rb1 = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        horizontalMove = Input.GetAxis("Joystick1Horizontal");
+    
+    
 
-        if(Input.GetButtonDown("Joystick1Jump") && IsGrounded())
-        {
-            rb1.velocity = new Vector2(rb1.velocity.x, jumpingPower);
-        }
-
-        if(Input.GetButtonDown("Joystick1Jump") && rb1.velocity.y > 0f)
-        {
-            rb1.velocity = new Vector2(rb1.velocity.x, rb1.velocity.y * 0.5f);
-        }
-    }
-
-    private void FixedUpdate()
-    {
-        rb1.velocity = new Vector2(horizontalMove * speed, rb1.velocity.y);
-    }
-
+   
     private bool IsGrounded()
     {
         return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        horizontalMove = context.ReadValue<Vector2>();
+        
+        rb1.velocity = new Vector2(horizontalMove.x * speed, rb1.velocity.y);
+    }
+
+    public void OnJump()
+    {
+        if(IsGrounded())
+        {
+            rb1.velocity = new Vector2(rb1.velocity.x, jumpingPower);
+        }
     }
 }
