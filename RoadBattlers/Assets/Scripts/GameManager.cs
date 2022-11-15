efe464ec665225d1bf0b9aedbcd404bfcfffb08b
Minty B <111585812+MintyTheCoder@users.noreply.github.com>
2022-11-15 15:52:41 -0500

    Update PlayerInput and InputHandler

---
+++ RoadBattlers/Assets/Scripts/GameManager.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject[] spawnPoints = new GameObject[2];
+
+    //list to store all players in the game
+    public List<PlayerInput> playerList = new List<PlayerInput>();
+
+    [SerializeField] InputAction joinAction;
+    [SerializeField] InputAction leaveAction;
+
+
+
+    // Instances of the Game Manager
+    public static GameManager gameManagerInstance = null;
+
+    //events
+    public event System.Action<PlayerInput> PlayerJoinedGame;
+    public event System.Action<PlayerInput> PlayerLeftGame;
+
+    private void Awake()
+    {
+        if(gameManagerInstance == null)
+        {
+            //if there is no game manager in the world
+            //then this will be the game manager
+            gameManagerInstance = this;
+        }
+        else if(gameManagerInstance != null)
+        {
+            //if there is already a game manager in the world
+           // Destroy it so there will not be more than one
+            Destroy(gameObject);
+        }
+
+        //look for player spawn points in the world and add them to the array
+       // spawnPoints[0] = GameObject.FindGameObjectWithTag("SpawnPointOne");
+        //spawnPoints[1] = GameObject.FindGameObjectWithTag("SpawnPointTwo");
+
+    }
+
+    private void Start()
+    {
+        //look for player spawn points in the world and add them to the array
+        spawnPoints[0] = GameObject.FindGameObjectWithTag("SpawnPointOne");
+        spawnPoints[1] = GameObject.FindGameObjectWithTag("SpawnPointTwo");
+
+        //add the first player to the world, no split screen, default input controls
+        PlayerInputManager.instance.JoinPlayer(0, -1, null);
+    }
+
+     //Input Manager Default method names
+     void OnPlayerJoined(PlayerInput playerInput)
+    {
+        //add the player to the list after they join the game
+        playerList.Add(playerInput);
+
+        //send the information to any other scripts that want the information
+        if(PlayerJoinedGame != null)
+        {
+            PlayerJoinedGame(playerInput);
+        }
+
+    }
+
+    void OnPlayerLeft(PlayerInput playerInput)
+    {
+
+    }
+}
---
+++ RoadBattlers/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float playerSpeed = 5.0f;
+    [SerializeField] private float jumpPower = 5.0f;
+
+    private Rigidbody2D playerRigidbody;
+
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask groundLayer;
+    private bool isTouchingGround;
+
+
+    Vector2 horizontalInput;
+
+
+    private void Start()
+    {
+        
+        playerRigidbody = GetComponent<Rigidbody2D>();
+        /*
+        if (_playerRigidbody == null)
+        {
+            Debug.LogError("Player is missing a Rigidbody2D component");
+        }
+        */
+    }
+
+    public void OnMove(InputAction.CallbackContext inputValue)
+    {
+        horizontalInput = inputValue.ReadValue<Vector2>();
+        playerRigidbody.velocity = new Vector2(horizontalInput.x * playerSpeed, playerRigidbody.velocity.y);
+    }
+
+    public void OnJump()
+    {
+        if ( IsGrounded())
+        {
+            playerRigidbody.velocity = new Vector2(0, jumpPower);
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        isTouchingGround = Physics2D.OverlapCircle(groundCheck.transform.position, groundCheckRadius, groundLayer);
+        return isTouchingGround;
+    }
+
+
+   public void OnAttack()
+   {
+        Debug.Log("Attack!");
+   }
+
+
+    private void Update()
+    {
+       // isTouchingGround = Physics2D.OverlapCircle(groundCheck.transform.position, groundCheckRadius, groundLayer);
+
+       // MovePlayer();
+
+    }
+    private void MovePlayer()
+    {
+        //var horizontalInput = Input.GetAxisRaw("Horizontal");
+        playerRigidbody.velocity = new Vector2(horizontalInput.x * playerSpeed, playerRigidbody.velocity.y);
+    }
+    // private void Jump() => _playerRigidbody.velocity = new Vector2(0, jumpPower);
+
+    
+}
