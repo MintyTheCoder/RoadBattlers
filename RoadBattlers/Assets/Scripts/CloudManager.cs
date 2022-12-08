3dd7df5f4addc4a1837ffb204f257632f7a22935
Minty B <111585812+MintyTheCoder@users.noreply.github.com>
2022-12-08 15:40:14 -0500

    Updated Clouds

--- RoadBattlers/Assets/Scripts/CloudManager.cs
+++ RoadBattlers/Assets/Scripts/CloudManager.cs
@@ -4,24 +4,32 @@ using UnityEngine;
 
 public class CloudManager : MonoBehaviour
 {
-    public GameObject SmallCloud;
-    public GameObject BigCloud;
-
-    Vector2 cloudPointOne = new Vector2(-2.5f, 1.5f);
+    public GameObject smallCloud;
+    public GameObject bigCloud;
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(BigCloud, cloudPointOne, Quaternion.identity);
-        
+        StartCoroutine("CloudSpawn");
     }
 
     public IEnumerator CloudSpawn()
-    { 
+    {
+        AddClouds();        
+        yield return new WaitForSeconds(1.25f);
+        StartCoroutine("CloudSpawn");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void AddClouds()
+    {
+        Vector2 cloudPointOne = new Vector2(-4.0f, 0.8f);
+        Vector2 cloudPointTwo = new Vector2(-4.5f, 1.5f);
+        Instantiate(smallCloud, cloudPointOne, Quaternion.identity);
+        Instantiate(bigCloud, cloudPointTwo, Quaternion.identity);
     }
 }
