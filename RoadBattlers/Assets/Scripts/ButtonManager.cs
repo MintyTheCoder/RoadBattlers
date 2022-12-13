fc574bca2c96ed0eb7dff838c5fe9c64da76f85d
Minty B <111585812+MintyTheCoder@users.noreply.github.com>
2022-12-13 15:51:32 -0500

    Updated Buttons and Pause Menu

---
+++ RoadBattlers/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//needed to switch scenes in Unity
+using UnityEngine.SceneManagement;
+public class ButtonManager : MonoBehaviour
+{
+    [SerializeField] private int currentScene;
+
+    //added for sounds
+    [SerializeField] AudioSource audioSource;
+    [SerializeField] AudioClip buttonClick;
+    private void Start()
+    {
+        currentScene = SceneManager.GetActiveScene().buildIndex;
+    }
+    public void GameStart()
+    {
+        //switch to next scene or level
+        currentScene++;//increase to next level number
+
+        //allows you to delay something from happening
+        StartCoroutine(ChangeToScene(currentScene));
+    }
+
+    public void GameQuit()
+    {
+        StartCoroutine(Quit());
+    }
+
+    public void Menu()
+    {
+        StartCoroutine(MainMenu());
+    }
+
+    public IEnumerator ChangeToScene(int changeSceneTo)
+    {
+        //add for sound effect
+        audioSource.PlayOneShot(buttonClick);
+        //wait a few seconds before switching to the next scene
+        yield return new WaitForSeconds(1);
+
+        //load the next scene
+        SceneManager.LoadScene(changeSceneTo);
+    }
+
+    public IEnumerator Quit()
+    {
+        //add for sound effect
+        audioSource.PlayOneShot(buttonClick);
+        //wait a few seconds before switching to the next scene
+        yield return new WaitForSeconds(1);
+
+        //load the next scene
+        Application.Quit();
+    }
+
+    public IEnumerator MainMenu()
+    {
+        //add for sound effect
+        audioSource.PlayOneShot(buttonClick);
+        //wait a few seconds before switching to the next scene
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene("Main Menu");
+        
+    }
+}
